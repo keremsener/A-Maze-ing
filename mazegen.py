1953ed6101a10dbef2e41e61eab67f3f290dd7c6
ykasaca <yusufemrekasaca@gmail.com>
2026-07-13 15:02:35 +0300

    1 - mazegen dosyası bitti ve mainine ufak bir test için ascii ile test kodu eklendi. 2 - Hexadecimal kaydedici yazıldı 1-2-4-8 mantığı kuruldu (test edilmedi)

--- mazegen.py
+++ mazegen.py
@@ -1,3 +1,4 @@
+
 import random
 
 class MazeGenerator:
@@ -23,4 +24,35 @@ class MazeGenerator:
                     self._dfs(nx, ny)
     
     def generate(self, entry_x: int, entry_y: int, exit_x: int, exit_y: int, perfect: int):
-        self._dfs(entry_x, entry_y)
+        self._dfs(1, 1)
+
+        self.grid[entry_y][entry_x] = 0
+        self.grid[exit_y][exit_x] = 0
+
+        if entry_x == 0:
+            self.grid[entry_y][1] = 0
+        elif entry_y == 0:
+            self.grid[1][entry_x] = 0
+
+        if exit_x == self.width - 1:
+            self.grid[exit_y][self.width - 2] = 0
+        elif exit_y == self.height - 1:
+            self.grid[self.height - 2][exit_x] = 0
+
+        if perfect == 0:
+            self._make_imperfect()
+
+        return self.grid
+
+    def _make_imperfect(self):
+        loop_count = (self.width * self.height) // 10
+        for _ in range(loop_count):
+            rx = random.randint(1, self.width - 2)
+            ry = random.randint(1, self.height - 2)
+            self.grid[ry][rx] = 0
+
+if __name__ == "__main__":
+    generator = MazeGenerator(21, 21)
+    grid = generator.generate(0, 0, 20, 20, 1)
+    for row in grid:
+        print("".join(["██" if cell == 1 else "  " for cell in row]))
