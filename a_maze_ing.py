da07cc660c183ad8379d94c2bf39e599d921d96f
Kerem Şener <blogkeremsener@gmail.com>
2026-07-24 01:45:23 +0300

    Refactor main_func to improve error handling and code structure

--- a_maze_ing.py
+++ a_maze_ing.py
@@ -1,46 +1,52 @@
 from mazegen import MazeGenerator
 from dotenv import load_dotenv
 import os
+import sys
 # pip install -e ./packages/mazegen
 
 
 def main_func():
-    load_dotenv()
-    raw_entry = os.getenv("ENTRY", "0,0")
-    entry_tuple = tuple(map(int, raw_entry.split(",")))
-    perfect_val = os.getenv("PERFECT", "True").lower() == "true"
-    raw_exit = os.getenv("EXIT", "3,3")
-    exit_tuple = tuple(map(int, raw_exit.split(",")))
+    try:
+        load_dotenv()
+        raw_entry = os.getenv("ENTRY", "0,0")
+        entry_tuple = tuple(map(int, raw_entry.split(",")))
+        perfect_val = os.getenv("PERFECT", "True").lower() == "true"
+        raw_exit = os.getenv("EXIT", "3,3")
+        exit_tuple = tuple(map(int, raw_exit.split(",")))
 
-    if os.getenv("PERFECT") == "True":
-        type = "PERFECT"
-    else:
-        type = "PAC-MAN"
+        if os.getenv("PERFECT") == "True":
+            type = "PERFECT"
+        else:
+            type = "PAC-MAN"
 
-    print(f"\n=== {type} MAZE ===")
-    perfect_maze = MazeGenerator(
-        width=int(os.getenv("WIDTH")),
-        height=int(os.getenv("HEIGHT")),
-        entry=entry_tuple,
-        exit=exit_tuple,
-        perfect=perfect_val)
-    perfect_maze.generate()
-    solution_path = perfect_maze.solve()
+        print(f"\n=== {type} MAZE ===")
+        perfect_maze = MazeGenerator(
+            width=int(os.getenv("WIDTH")),
+            height=int(os.getenv("HEIGHT")),
+            entry=entry_tuple,
+            exit=exit_tuple,
+            perfect=perfect_val)
+        perfect_maze.generate()
+        solution_path = perfect_maze.solve()
+    
+        for y in range(perfect_maze.height):
+            row_str = []
+            for x in range(perfect_maze.width):
+                if (x, y) == perfect_maze.entry:
+                    row_str.append("EN")
+                elif (x, y) in perfect_maze.pattern_cells:
+                    row_str.append("XX")
+                elif (x, y) == perfect_maze.exit:
+                    row_str.append("EX")
+                elif (x, y) in solution_path:
+                    row_str.append("..")
+                else:
+                    row_str.append(f"{perfect_maze.grid[y][x]:2}")
+            print("  ".join(row_str))
 
-    for y in range(perfect_maze.height):
-        row_str = []
-        for x in range(perfect_maze.width):
-            if (x, y) == perfect_maze.entry:
-                row_str.append("EN")
-            elif (x, y) in perfect_maze.pattern_cells:
-                row_str.append("XX")
-            elif (x, y) == perfect_maze.exit:
-                row_str.append("EX")
-            elif (x, y) in solution_path:
-                row_str.append("..")
-            else:
-                row_str.append(f"{perfect_maze.grid[y][x]:2}")
-        print("  ".join(row_str))
+    except Exception as e:
+        print(f"Error: {e}")
+        sys.exit(1)
 
 
 if __name__ == "__main__":
