915345e87830fb47c86f64db039c70827522106d
ksener <blogkeremsener@gmail.com>
2026-07-24 14:36:54 +0300

    Added: while(True) loop.

--- a_maze_ing.py
+++ a_maze_ing.py
@@ -4,8 +4,11 @@ import os
 import sys
 # pip install -e ./packages/mazegen
 
+maze_counter = 0
+
 
 def main_func():
+    global maze_counter
     try:
         load_dotenv()
         raw_entry = os.getenv("ENTRY", "0,0")
@@ -26,6 +29,7 @@ def main_func():
             entry=entry_tuple,
             exit=exit_tuple,
             perfect=perfect_val)
+        maze_counter += 1
         perfect_maze.generate()
         solution_path = perfect_maze.solve()
         for y in range(perfect_maze.height):
@@ -42,7 +46,21 @@ def main_func():
                 else:
                     row_str.append(f"{perfect_maze.grid[y][x]:2}")
             print("  ".join(row_str))
-
+        while True:
+            print("=== A-Maze-ing ===")
+            print("""
+1. Re-generate a new maze
+2. Show/Hide the shortest path
+3. Rotate the wall colours
+4. Quit
+""")
+            question = int(input("Choice? (1-4):"))
+            if question == 1:
+                main_func()
+            elif question == 4:
+                print(f"Total number of mazes generated: {maze_counter}."
+                      f" Program closing..")
+                sys.exit(0)
     except Exception as e:
         print(f"Error: {e}")
         sys.exit(1)
